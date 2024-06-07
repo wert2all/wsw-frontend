fd0af52202e9589165b25fe057ac573b34d40ade
wert2all <wert2all@gmail.com>
2024-06-08 01:36:48 +0300

    feat: add initial effect for init token

--- src/app/store/preview/preview.effects.ts
+++ src/app/store/preview/preview.effects.ts
@@ -1,1 +1,16 @@
-export const previewEffects = {};
+import { inject } from '@angular/core';
+import {
+  Actions,
+  createEffect,
+  ofType,
+  ROOT_EFFECTS_INIT,
+} from '@ngrx/effects';
+
+import { StoreUnDispatchEffect } from '../../app.types';
+
+const initState = (actions$ = inject(Actions)) =>
+  actions$.pipe(ofType(ROOT_EFFECTS_INIT));
+
+export const previewEffects = {
+  initState: createEffect(initState, StoreUnDispatchEffect),
+};
