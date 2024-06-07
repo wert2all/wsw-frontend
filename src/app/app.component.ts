45677ed46423f3b7d56577ee4250ee79f959401e
wert2all <wert2all@gmail.com>
2024-06-07 15:43:21 +0300

    feat: add input component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { InputUrlComponent } from './input-url/input-url.component';
 import { TitleComponent } from './share/content/title/title.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [RouterOutlet, TitleComponent],
+  imports: [RouterOutlet, TitleComponent, InputUrlComponent],
 })
 export class AppComponent {}
