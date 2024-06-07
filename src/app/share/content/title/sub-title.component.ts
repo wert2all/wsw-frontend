165f52f5d1fb09f70f61cca0dccf73b11f67ce82
wert2all <wert2all@gmail.com>
2024-06-07 16:40:58 +0300

    feat: add simple  code container

---
+++ src/app/share/content/title/sub-title.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-share-content-sub-title',
+  standalone: true,
+  imports: [CommonModule],
+  template: `<h1 class="default-font-bold text-2xl">
+    <ng-content></ng-content>
+  </h1>`,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SubTitleComponent {}
