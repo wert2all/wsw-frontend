2a829a2d062388e3dcbdf273eb36d0984d9f79f8
wert2all <wert2all@gmail.com>
2024-06-11 12:37:18 +0300

    feat: add codegen

---
+++ codegen.ts
@@ -0,0 +1,45 @@
+import type { CodegenConfig } from '@graphql-codegen/cli';
+import { environment } from './src/environments/environment';
+
+const config: CodegenConfig = {
+  overwrite: true,
+  schema: environment.graphqlUrl,
+  documents: ['./src/app/api/**/*.graphql'],
+  emitLegacyCommonJSImports: false,
+  generates: {
+    './src/app/api/schema.graphql': {
+      plugins: ['schema-ast'],
+    },
+    './src/app/api/graphql.ts': {
+      plugins: [
+        'typescript',
+        'typescript-operations',
+        'typescript-apollo-angular',
+      ],
+      config: {
+        addExplicitOverride: true,
+        omitOperationSuffix: true,
+        querySuffix: 'Query',
+        mutationSuffix: 'Mutation',
+        serviceName: 'ApiClient',
+        sdkClass: true,
+        skipTypename: true,
+
+        skipTypeNameForRoot: true,
+        preResolveTypes: true,
+        declarationKind: 'interface',
+        onlyOperationTypes: true,
+        inlineFragmentTypes: 'combine',
+
+        namingConvention: {
+          enumValues: 'keep',
+        },
+        strictScalars: true,
+        scalars: {},
+      },
+    },
+  },
+  hooks: { afterAllFileWrite: ['prettier --write'] },
+};
+
+export default config;
