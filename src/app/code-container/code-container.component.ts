d5a2f77766ddb6b2eef32da78b952032dd6223ea
wert2all <wert2all@gmail.com>
2024-06-12 21:39:18 +0300

    feat: add token to code component

--- src/app/code-container/code-container.component.ts
+++ src/app/code-container/code-container.component.ts
@@ -1,6 +1,13 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  inject,
+  input,
+} from '@angular/core';
 
+import { ApiUrlService } from '../service/api-url.service';
 import { SubTitleComponent } from '../share/content/title/sub-title.component';
 import { TitleComponent } from '../share/content/title/title.component';
 
@@ -12,5 +19,20 @@ import { TitleComponent } from '../share/content/title/title.component';
   imports: [CommonModule, TitleComponent, SubTitleComponent],
 })
 export class CodeContainerComponent {
+  private readonly apiUrlService = inject(ApiUrlService);
   token = input.required<string | undefined>();
+
+  imgUrl = computed(() => {
+    const token = this.token();
+    return token ? this.apiUrlService.createApiImageUrl(token) : undefined;
+  });
+
+  jsonUrl = computed(() => {
+    const token = this.token();
+    return token ? this.apiUrlService.createApiJsonUrl(token) : undefined;
+  });
+
+  gqlUrl = computed(() => {
+    return this.apiUrlService.createApiGqlUrl();
+  });
 }
