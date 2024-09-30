d8eadc88997fbb1268d7aee8a1d3269e0371d165
Serhii Shvets <wert2all@gmail.com>
2024-09-30 16:02:36 +0300

    feat: add footer
    feat: add a footer

---
+++ src/app/share/footer/footer.component.ts
@@ -0,0 +1,16 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import { phosphorGithubLogo } from '@ng-icons/phosphor-icons/regular';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [CommonModule, NgIconComponent],
+  templateUrl: './footer.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  viewProviders: [provideIcons({ phosphorGithubLogo })],
+})
+export class FooterComponent {
+  currentYear: string = new Date().getFullYear().toString();
+}
